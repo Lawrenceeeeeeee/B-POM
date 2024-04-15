cbc3d98f6f88b21d7c0ee3c7437c96bb6401e5ba
CLDYG7240 <1642933673@qq.com>
2024-04-15 20:25:35 +0800

    example文件上传

--- DaBiao.py
+++ DaBiao.py
@@ -23,11 +23,14 @@ def update_csv_with_emotions(input_filename, output_filename):
         headers = next(reader, None)  # 获取列名
         if headers:
             content_index = headers.index('content')  # 获取"content"列的索引
+            headers.extend(['emotion', 'justice'])  # 添加新的列标题
         else:
             print("警告：CSV文件没有列名。")
             return
         with open(output_filename, mode='a' if start_row > 0 else 'w', newline='', encoding='utf-8') as outfile:
             writer = csv.writer(outfile)
+            if start_row == 0:  # 如果从头开始，写入列标题
+                writer.writerow(headers)
             for i, row in enumerate(reader, start=1):
                 if i <= start_row:  # 跳过已经处理过的行
                     continue
