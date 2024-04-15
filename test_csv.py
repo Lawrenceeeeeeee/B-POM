b44b64f1edf8a32050fbed08955c3f18406fbf98
CLDYG7240 <1642933673@qq.com>
2024-04-15 17:59:03 +0800

    删除生成测试文件文件

--- test_csv.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open("test.csv","w") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow([20221111,'fgsdhsdfhsdfh'])
-    writer.writerow([20221324,'aaaaaasdafasfasf'])
-    writer.writerow([20221421,'afeggdgdfafd'])
