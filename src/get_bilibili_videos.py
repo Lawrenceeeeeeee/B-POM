dc60a23ab75ad8640e5959caf99abf0a3b382a59
Lawrenceeeeeeee <775569157@qq.com>
2024-04-17 23:01:01 +0800

    调整summary

--- src/get_bilibili_videos.py
+++ src/get_bilibili_videos.py
@@ -1,6 +1,11 @@
 import requests
 import json
 import subprocess
+import os
+import transcribe as tr
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 SESSDATA = 'f7d22332%2C1728481618%2Cf242a%2A41CjBLPbnBWSHKva1n24Wnxt-G4dxI7CA89KIX2tcS7zVz6VFvUVwj-hvU96ZCR68-kFESVjlsc0NOMi10ZUNpRGFNRnJyNXNHTEpLbzMzaTJSeV93aGpKRFRLUk5wRFpLdzFvcjFRZFZ6alAzLUJkZkVIUTFVMVh2WjVJV1p6eXhrVUhqelVIc3lRIIEC'
 
@@ -66,19 +71,63 @@ def get_dash_urls(bvid, cid, quality=32):
     return video_url, audio_url
 
 def download_file(url, output_filename):
-    command = ['wget', url, '--referer', 'https://www.bilibili.com', '-O', f"{output_filename}.m4a"]
+    command = ['wget', url, '--referer', 'https://www.bilibili.com', '-O', f"temp/{output_filename}.m4a"]
     # 通过ffmpeg转成wav
     
     subprocess.run(command)
-    command = ['ffmpeg', '-i', f"{output_filename}.m4a", '-ar', '16000', output_filename + '.wav']
+    command = ['ffmpeg', '-i', f"./temp/{output_filename}.m4a", '-ar', '16000', f'./temp/{output_filename}.wav']
+    
 
     subprocess.run(command)
+    os.remove(f"temp/{output_filename}.m4a")
+    
+def summarize(text, model="gpt-4-turbo"):
+    system_prompt = """
+    你现在是一个AI总结概括小助手，专门对视频的内容进行总结。用户会传入视频的自动生成字幕的内容（包含每个字幕的时间戳，每一行字幕结构为："[timestamp] subtitle"），你要做的任务是：
+     - 先对整个视频的主要内容进行概括，将字数控制在100字以内
+     - 然后根据视频内容，将视频划分成几个大部分（如果视频太短就不需要分太多），并对每一个部分进行简要概括，每一个部分的概括不超过50字
+     
+     输出参考如下：
+     ```
+     {
+            "summary": "这是一段科普视频，讲解了……（100字以内）",
+            "parts": [
+                "这是第一部分……（50字以内）",
+                "这是第二部分……（50字以内）",
+                "这是结尾……（50字以内）",
+                ……
+            ]
+     }
+     ```
+    """
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": text},
+        ]
+    )
+    res = response.choices[0].message.content
+    return res
 
 if __name__ == '__main__':
-    # 【【官方双语】深度学习之神经网络的结构 Part 1 ver 2.0】 https://www.bilibili.com/video/BV1bx411M7Zx/?share_source=copy_web&vd_source=9e94008dbf76e399a164028430118348
-    bvid = 'BV1bx411M7Zx'
-    cid = 25368631
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    original_cwd = os.getcwd()
+    # 更改工作目录到函数所在文件目录
+    os.chdir(current_dir)
+    
+    
+    bvid = 'BV11c411z7PL'
+    cid = get_cid(bvid)[0]
     audio_url = get_dash_urls(bvid, cid)[1]
     # print(json.dumps(get_dash_urls(bvid, cid), indent=4))
     # print(json.dumps(get_cid(av=bvid), indent=4))
     download_file(audio_url, bvid)
+    content = tr.transcribe(f"temp/{bvid}.wav")
+    # print(content)
+    os.remove(f"temp/{bvid}.wav")
+    res = summarize(content)
+    print(res)
+    
+    os.chdir(original_cwd)
+    
--- src/transcribe.py
+++ src/transcribe.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-def transcribe(video_path, output_path):
+def transcribe(video_path):
     video_file = os.path.basename(video_path)
     video_name = os.path.splitext(video_file)[0]
     # 获取当前函数所在文件的目录
@@ -11,8 +11,7 @@ def transcribe(video_path, output_path):
     # 更改工作目录到函数所在文件目录
     os.chdir(current_dir)
     
-    output_path = "temp"
-    command = ['../whisper.cpp/main', '-m', '../whisper.cpp/models/ggml-small.bin', '-f', '../' + video_path, '-of', f'temp/{video_name}', '-olrc', 'true']
+    command = ['../whisper.cpp/main', '-m', '../whisper.cpp/models/ggml-medium.bin', '-f', video_path, '-of', f'temp/{video_name}', '-olrc', 'true', '-l', 'auto', '--prompt', '这是一段视频的音频，如果是中文请用简体中文输出。']
     # subprocess.call(command, shell=True)
     subprocess.run(command)
     output_path = f'temp/{video_name}.lrc'
